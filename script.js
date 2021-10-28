1d2cc52b962507e4ee16d883197cf85556221632
Viktar Kolasau <vitykolosov@gmail.com>
2021-10-28 19:33:18 +0200

    add some fixes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const menuBtn = document.querySelector('.menu-btn');
 const overlayMenuElement = document.querySelector('.menu-overlay');
 const overlayNavigationElement = document.querySelector('.menu-overlay__navigation-list');
+const footerText = document.querySelector('.edge__text');
 let menuOpen = false;
+
+footerText.textContent = `© ${new Date().getFullYear()} woodoo barbershop`;
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('open');
