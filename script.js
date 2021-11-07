7a9ffb3f87f36f458d1d5a46aed67fa4b5822096
Viktar Kolasau <vitykolosov@gmail.com>
2021-11-07 16:43:34 +0100

    feat: implement gallery image modal

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const menuBtn = document.querySelector('.menu-btn');
 const overlayMenuElement = document.querySelector('.menu-overlay');
 const overlayNavigationElement = document.querySelector('.menu-overlay__navigation-list');
 const footerText = document.querySelector('.edge__text');
+const modal = document.querySelector('.gallery-modal');
+const modalImg = document.querySelector('.gallery-modal__content');
+const modalCloseBtn = document.querySelector('.gallery-modal__close-btn');
+const galleryImages = document.querySelectorAll('.gallery__image');
+
 let menuOpen = false;
 
+//Footer copyright date
 footerText.textContent = `© ${new Date().getFullYear()} woodoo barbershop`;
 
+//Burger menu
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('open');
   overlayMenuElement.classList.toggle('active');
@@ -19,3 +26,18 @@ overlayNavigationElement.addEventListener('click', (event) => {
     menuOpen = !menuOpen;
   }
 });
+
+//Gallery modal
+modal.onclick = () => {
+  modal.classList.add('hidden');
+}
+
+galleryImages.forEach((img) => img.onclick = () => {
+  modal.classList.remove('hidden');
+  modalImg.src = img.src;
+  modalImg.onclick = (event) => event.stopPropagation();
+});
+
+modalCloseBtn.onclick = () => {
+  modal.classList.add('hidden');
+}
