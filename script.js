ebb088d293a6ff2bb304138d6c3b76869e781278
Viktar Kolasau <vitykolosov@gmail.com>
2021-11-23 00:03:06 +0100

    feat: add gallery slider to gallery modal

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const overlayMenuElement = document.querySelector('.menu-overlay');
 const overlayNavigationElement = document.querySelector('.menu-overlay__navigation-list');
 const footerText = document.querySelector('.edge__text');
 const modal = document.querySelector('.gallery-modal');
-const modalImg = document.querySelector('.gallery-modal__content');
 const modalCloseBtn = document.querySelector('.gallery-modal__close-btn');
 const galleryImages = document.querySelectorAll('.gallery__image');
-
 let menuOpen = false;
 
 //Footer copyright date
@@ -27,21 +25,6 @@ overlayNavigationElement.addEventListener('click', (event) => {
   }
 });
 
-//Gallery modal
-modal.onclick = () => {
-  modal.classList.add('hidden');
-}
-
-galleryImages.forEach((img) => img.onclick = () => {
-  modal.classList.remove('hidden');
-  modalImg.src = img.src;
-  modalImg.onclick = (event) => event.stopPropagation();
-});
-
-modalCloseBtn.onclick = () => {
-  modal.classList.add('hidden');
-}
-
 //Jquery script for links smoothing
 $(document).ready(function(){
   $("a").on('click', function(event) {
@@ -57,3 +40,54 @@ $(document).ready(function(){
     } 
   });
 });
+
+//Gallery modal
+modal.onclick = (e) => {
+  if (e.target.classList.contains('gallery-modal')) {
+    modal.classList.add('hidden');
+  }
+}
+
+galleryImages.forEach((img) => img.onclick = () => {
+  modal.classList.remove('hidden');
+});
+
+modalCloseBtn.onclick = () => {
+  modal.classList.add('hidden');
+}
+
+//Splide gallery
+document.addEventListener( 'DOMContentLoaded', function() {
+    let splide = new Splide( '#main-slider', {
+      pagination: false,
+      type: 'loop',
+    } );
+
+    let thumbnails = document.getElementsByClassName( 'thumbnail' );
+    let current;
+
+    for ( let i = 0; i < thumbnails.length; i++ ) {
+      initThumbnail( thumbnails[ i ], i );
+    }
+
+    function initThumbnail( thumbnail, index ) {
+      thumbnail.addEventListener( 'click', function () {
+        splide.go( index );
+      } );
+    }
+
+    splide.on( 'mounted move', function () {
+      let thumbnail = thumbnails[ splide.index ];
+
+      if ( thumbnail ) {
+        if ( current ) {
+          current.classList.remove( 'is-active' );
+        }
+
+        thumbnail.classList.add( 'is-active' );
+        current = thumbnail;
+      }
+    } );
+
+    splide.mount();
+  } );
